a27b2a97d6332d9ae772b2da69c4f4fb1b4e56c0
Dave Campbell <dave@digetl.co.uk>
2020-06-16 19:42:39 +0100

    Book has name - passed

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -17,14 +17,14 @@ public class BookTest {
         book1 = new Book("The Beast of Buckingham Palace", "David Walliams", "Childrens Fiction");
         book2 = new Book("Slime", "David Walliams", "Childrens Fiction");
         book3 = new Book("Fing", "David Walliams", "Childrens Fiction");
-        book2 = new Book("Dog Zombies Rule", "Liz Pichon", "Childrens Fiction");
-        book2 = new Book("Spectacular School Trip", "Liz Pichon", "Non Fiction");
-        book2 = new Book("Everthing's Amazing", "Liz Pichon", "Autobiography");
+        book4 = new Book("Dog Zombies Rule", "Liz Pichon", "Childrens Fiction");
+        book5 = new Book("Spectacular School Trip", "Liz Pichon", "Non Fiction");
+        book6 = new Book("Everthing's Amazing", "Liz Pichon", "Autobiography");
     }
 
         @Test
         public void bookHasTitle(){
-        assertEquals("Slime", book2.hasName());
+        assertEquals("Slime", book2.getTitle());
         }
 
 
